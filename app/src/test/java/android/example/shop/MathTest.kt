248980b87f52979a388534bcd9e6bc4485a3f2cb
pink production <sanjaplushka@gmail.com>
2020-03-29 01:17:58 +0500

    Добавил тест dividing_test

--- app/src/test/java/android/example/shop/MathTest.kt
+++ app/src/test/java/android/example/shop/MathTest.kt
@@ -1,2 +1,14 @@
 package android.example.shop
 
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class MathTest {
+
+    @Test
+    fun dividing_test() {
+        var i = 4
+        var j = 2
+        assertEquals(i / j, 2)
+    }
+}
