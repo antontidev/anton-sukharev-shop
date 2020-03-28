b4b29b34e28e6eea2739102ab49e416a78571993
pink production <sanjaplushka@gmail.com>
2020-03-29 01:14:06 +0500

    Добавил тест increment_test

--- app/src/test/java/android/example/shop/MathTest.kt
+++ app/src/test/java/android/example/shop/MathTest.kt
@@ -1,2 +1,14 @@
 package android.example.shop
 
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class MathTest {
+
+    @Test
+    fun increment_test() {
+        var i = 2
+        i++
+        assertEquals(3, i)
+    }
+}
