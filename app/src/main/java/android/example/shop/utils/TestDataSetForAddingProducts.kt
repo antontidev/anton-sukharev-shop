924dc180c150cd1aa73077bf9439bece5249f444
Anton <sanjaplushka@gmail.com>
2020-04-25 20:14:51 +0500

    Revert "Мелкие поправки"
    This reverts commit f4fbfbed

--- app/src/main/java/android/example/shop/utils/TestDataSetForAddingProducts.kt
+++ app/src/main/java/android/example/shop/utils/TestDataSetForAddingProducts.kt
@@ -6,7 +6,7 @@ import kotlinx.android.synthetic.main.shopping_cart_fragment.view.*
 import kotlin.random.Random
 
 class TestDataSetForAddingProducts {
-    private val data = mutableListOf(TestShoppingCartItemModel(name = "Leagoo Power",
+    val data = mutableListOf(TestShoppingCartItemModel(name = "Leagoo Power",
             description = "Cheap chinese phone.",
             fullDescription = "Long battery life. Good camera. Big display. New Phone.",
             price = 7000.0,
@@ -19,32 +19,11 @@ class TestDataSetForAddingProducts {
             discount = 15.0,
             id = R.drawable.ic_error),
         TestShoppingCartItemModel(name = "Apple Iphone 11",
-            description = "New Apple phone",
-            fullDescription = "Good camera, cpu and in-app performance. New cpu Apple A13.",
+            description = "",
+            fullDescription = "",
             price = 110000.0,
             discount = 10.0,
-            id = R.drawable.ic_error),
-        TestShoppingCartItemModel(name = "Iphone 8",
-            description = "Beautiful phone",
-            fullDescription = "Best phone ever made. Add it to shopping cart",
-            price = 25000.0,
-            discount = 20.0,
-            id = R.drawable.common_full_open_on_phone
-        ),
-        TestShoppingCartItemModel(name = "Samsung S20",
-            description = "New Samsung phone",
-            fullDescription = "Cool phone with branch of brand new features. Good camera.",
-            price = 80000.0,
-            discount = 5.0,
-            id = R.drawable.common_google_signin_btn_icon_dark
-        ),
-        TestShoppingCartItemModel(name = "Meizu m6 note",
-            description = "Good chinese phone.",
-            fullDescription = "Not so good as you mentioned. Best choice for grandmas. You can buy it at your own risk.",
-            price = 10000.0,
-            discount = 10.0,
-            id = R.drawable.fui_ic_microsoft_24dp
-        )
+            id = R.drawable.ic_error)
     )
 
     fun getNextItem(): TestShoppingCartItemModel {
